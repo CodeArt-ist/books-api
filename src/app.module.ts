9543bb844d59ed9d58f8a3599dc1d3dfbd934955
suleyman <suleyman.sevimli1@gmail.com>
2021-02-10 10:01:46 +0300

    remove unused imported entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,8 @@ import { Module } from '@nestjs/common';
 import { ReviewsModule } from './reviews/reviews.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './auth/entities/user.entity';
-import { Reviews } from './reviews/entities/reviews.entity';
 import { BookModule } from './book/book.module';
 import * as config from 'config';
-import { Book } from './book/entities/book.entity';
 
 const dbConfig = config.get('db');
 
